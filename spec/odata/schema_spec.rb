ad92f4e498b03729b3ceadcbacd8a97e267732ba
Corentin Descamps <corentin.descamps@epitech.eu>
2020-09-30 15:24:46 +0200

    Adding specs for the Schema class and using the load_xml_document helper method

--- spec/odata/schema_spec.rb
+++ spec/odata/schema_spec.rb
@@ -1,9 +1,11 @@
 require 'spec_helper'
 
 describe OData::Schema do
-  let(:doc) { File.open("#{RSPEC_ROOT}/fixtures/sample_service/metadata.xml") { |f| Nokogiri::XML(f) }.remove_namespaces! }
+  let(:doc) { load_xml_document("#{RSPEC_ROOT}/fixtures/sample_service/metadata.xml") }
   let(:schema_xml) { doc.xpath("//Schema").first }
   let(:schema) { OData::Schema.new(schema_xml, nil) }
 
   it { expect(schema.namespace).to eq "ODataDemo" }
+
+  it { expect(schema.entity_types).to eq ["Product", "FeaturedProduct", "ProductDetail", "Category", "Supplier", "Person", "Customer", "Employee", "PersonDetail", "Advertisement"] }
 end
