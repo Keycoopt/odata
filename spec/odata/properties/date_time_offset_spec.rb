14c466693c6d309bbe90fd76270a6c07558314d3
Corentin Descamps <corentin.descamps@epitech.eu>
2020-10-02 16:52:03 +0200

    Fix the DateTimeOffset (#3)
    * Fix the DateTime and DateTimeOffset specs to fit the version 2.0 of the protocol
    Adding a check about nil values for DateTimeOffset and that the allows_nil option raise an error if we try to assign a nil value
    
    * Fix the DateTimeOffset implementation to fit the 2.0 spec of the property
    Also fixes the checks for allows_nil (typo) and the implementation (return nil in the parsing, validation if allows_nil? is true)
    
    * Mutualise the implementation of DateTime and DateTimeOffset by extracting the format into a method

--- spec/odata/properties/date_time_offset_spec.rb
+++ spec/odata/properties/date_time_offset_spec.rb
@@ -1,11 +1,11 @@
 require 'spec_helper'
 
 describe OData::Properties::DateTimeOffset do
-  let(:subject) { OData::Properties::DateTimeOffset.new('DateTime', '2000-01-01T16:00:00.000Z-09:00') }
-  let(:new_datetime) { DateTime.strptime('2004-05-01T14:32:00.000Z+02:00', '%Y-%m-%dT%H:%M:%S.%LZ%:z') }
+  let(:subject) { OData::Properties::DateTimeOffset.new('DateTime', '2002-10-10T17:00:00Z') }
+  let(:new_datetime) { DateTime.strptime('2015-01-08T22:11:58Z', '%Y-%m-%dT%H:%M:%S%:z') }
 
   it { expect(subject.type).to eq('Edm.DateTimeOffset') }
-  it { expect(subject.value).to eq(DateTime.strptime('2000-01-01T16:00:00.000Z-09:00', '%Y-%m-%dT%H:%M:%S.%LZ%:z')) }
+  it { expect(subject.value).to eq(DateTime.strptime('2002-10-10T17:00:00Z', '%Y-%m-%dT%H:%M:%S%:z')) }
 
   it { expect {subject.value = 'bad'}.to raise_error(ArgumentError) }
 
@@ -13,4 +13,16 @@ describe OData::Properties::DateTimeOffset do
     subject.value = new_datetime
     subject.value
   }.call).to eq(new_datetime) }
+
+  it { expect(lambda {
+    subject.value = nil
+  }).not_to raise_error }
+
+  context "with the allows_nil option set to false" do
+    let(:subject) { OData::Properties::DateTimeOffset.new('DateTime', '2002-10-10T17:00:00Z', allows_nil: false) }
+
+    it { expect(lambda {
+      subject.value = nil
+    }).to raise_error(ArgumentError) }
+  end
 end
