0d363590e7010b6d81f0a9d312d5f01d77a36fbd
Corentin Descamps <corentin.descamps@epitech.eu>
2020-10-02 15:02:31 +0200

    Mutualise the implementation of DateTime and DateTimeOffset by extracting the format into a method

--- lib/odata/properties/date_time_offset.rb
+++ lib/odata/properties/date_time_offset.rb
@@ -1,23 +1,7 @@
 module OData
   module Properties
     # Defines the DateTimeOffset OData type.
-    class DateTimeOffset < OData::Property
-      # Returns the property value, properly typecast
-      # @return [DateTime,nil]
-      def value
-        if (@value.nil? || @value.empty?) && allows_nil?
-          nil
-        else
-          ::DateTime.strptime(@value, '%Y-%m-%dT%H:%M:%S%:z')
-        end
-      end
-
-      # Sets the property value
-      # @params new_value [DateTime]
-      def value=(new_value)
-        validate(new_value)
-        @value = parse_value(new_value)
-      end
+    class DateTimeOffset < OData::Properties::DateTime
 
       # The OData type name
       def type
@@ -26,18 +10,8 @@ module OData
 
       private
 
-      def validate(value)
-        unless value.is_a?(::DateTime) || (value.nil? || value.empty?) && allows_nil?
-          raise ArgumentError, 'Value is not a date time format that can be parsed'
-        end
-      end
-
-      def parse_value(value)
-        return value if value.nil? && allows_nil?
-
-        parsed_value = value
-        parsed_value = ::DateTime.parse(value) unless value.is_a?(::DateTime)
-        parsed_value.strftime('%Y-%m-%dT%H:%M:%S%:z')
+      def strptime_format
+        '%Y-%m-%dT%H:%M:%S%:z'
       end
     end
   end
